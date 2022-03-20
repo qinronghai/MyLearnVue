61d26e2a281b2f93723b5cf8af694be7048d2154
SeaEliy <185043843@qq.com>
2022-03-20 10:55:32 +0800

    master

---
+++ 20-src-github搜索案例/main.js
@@ -0,0 +1,19 @@
+// 引入Vue
+import Vue from "vue";
+// 引入App
+import App from './App.vue'
+// 关闭Vue的生产提示
+Vue.config.productionTip = false;
+
+// Vue.prototype.x = { a: 1, b: 2 };
+
+
+
+// 创建vm
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  beforeCreate() {
+    Vue.prototype.$bus = this // 安装全局事件总线
+  },
+})
