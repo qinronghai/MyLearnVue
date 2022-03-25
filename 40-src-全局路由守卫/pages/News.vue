e02a4e7455509e0985b64c7b7762e7fb01d0ee1f
SeaEliy <185043843@qq.com>
2022-03-26 07:59:10 +0800

    master

---
+++ 40-src-全局路由守卫/pages/News.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul>
+      <li :style="{ opacity }">欢迎学习Vue</li>
+      <li>news001 <input type="text" /></li>
+      <li>news002 <input type="text" /></li>
+      <li>news003 <input type="text" /></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "News",
+  data() {
+    return {
+      opacity: 1,
+    };
+  },
+  /* beforeDestroy() {
+    console.log("News组件即将被销毁");
+    clearInterval(this.timer);
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      console.log('@');
+      this.opacity -= 0.01;
+      if (this.opacity <= 0) this.opacity = 1;
+    }, 16);
+  }, */
+
+  // 激活
+  /* activated() {
+    console.log('New组件被激活了');
+    this.timer = setInterval(() => {
+      console.log('@');
+      this.opacity -= 0.01;
+      if (this.opacity <= 0) this.opacity = 1;
+    }, 16);
+  },
+  deactivated() {
+    console.log('New组件失活了');
+    clearInterval(this.timer);
+  }, */
+};
+</script>
+
+<style>
+</style>
